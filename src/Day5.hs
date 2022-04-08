d175abe71b37a13a0b78575b073e9009f84c8d52
Mikhail Brinchuk <thecentury@gmail.com>
2022-04-08 23:20:01 +0200

    Day 5 is finished

--- src/Day5.hs
+++ src/Day5.hs
@@ -27,19 +27,30 @@ data Line2d =
 data ClassifiedLine =
   Horizontal { x1 :: Int, x2 :: Int, y :: Int }
   | Vertical { y1 :: Int, y2 :: Int, x :: Int }
-  | Other Line2d
+  | Diagonal Line2d
   deriving (Show, Eq)
 
 classifyLine :: Line2d -> ClassifiedLine
 classifyLine l@(Line2d (Point x1 y1) (Point x2 y2))
   | y1 == y2 = Horizontal (min x1 x2) (max x1 x2) y1
   | x1 == x2 = Vertical (min y1 y2) (max y1 y2) x1
-  | otherwise = Other l
+  | otherwise = Diagonal l
+
+coveredPointsNoDiagonal :: ClassifiedLine -> [Point]
+coveredPointsNoDiagonal (Horizontal x1 x2 y) = [Point x y | x <- [x1..x2]]
+coveredPointsNoDiagonal (Vertical y1 y2 x) = [Point x y | y <- [y1..y2]]
+coveredPointsNoDiagonal (Diagonal _) = []
 
 coveredPoints :: ClassifiedLine -> [Point]
 coveredPoints (Horizontal x1 x2 y) = [Point x y | x <- [x1..x2]]
 coveredPoints (Vertical y1 y2 x) = [Point x y | y <- [y1..y2]]
-coveredPoints (Other _) = []
+coveredPoints (Diagonal (Line2d (Point x1 y1) (Point x2 y2))) =
+  let
+    len = abs (x2 - x1)
+    dx = if x1 < x2 then 1 else -1
+    dy = if y1 < y2 then 1 else -1
+  in
+    [Point x y | d <- [0..len], let x = x1 + d * dx, let y = y1 + d * dy]
 
 --------------------------------------------------------------------------------
 
@@ -65,6 +76,11 @@ regularParse p = parse p ""
 
 --------------------------------------------------------------------------------
 
+atLeast2LinesOverlapNoDiagonal :: [Line2d] -> Int
+atLeast2LinesOverlapNoDiagonal inputLines = length $ filter (\g -> length g >= 2) covered where
+  classified = map classifyLine inputLines
+  covered = group $ sort $ concatMap coveredPointsNoDiagonal classified
+
 atLeast2LinesOverlap :: [Line2d] -> Int
 atLeast2LinesOverlap inputLines = length $ filter (\g -> length g >= 2) covered where
   classified = map classifyLine inputLines
@@ -77,7 +93,13 @@ run = do
   content <- readFile "input/day5.txt"
   let lines' = mapM (regularParse lineParser) (lines content)
   case lines' of
-    Right lines'' -> let count = atLeast2LinesOverlap lines'' in
-      putStrLn $ "There are " ++ show count ++ " lines that overlap at least 2 times"
+    Right lines'' ->
+      let
+        countNoDiagonal = atLeast2LinesOverlapNoDiagonal lines''
+        countWithDiagonal = atLeast2LinesOverlap lines''
+      in
+        do
+          putStrLn $ "There are " ++ show countNoDiagonal ++ " lines that overlap at least 2 times (excluding diagonals)"
+          putStrLn $ "There are " ++ show countWithDiagonal ++ " lines that overlap at least 2 times"
     Left err -> print err
   return ()
