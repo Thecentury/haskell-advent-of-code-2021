33755d849a39b3ef063a9ebcd584dfd86480acce
Mikhail Brinchuk <thecentury@gmail.com>
2022-04-14 18:19:47 +0200

    As I assume, finished Day 8 part 1

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,14 +1,19 @@
 module Day8 where
 
 import Text.Parsec
-import Text.Parsec.String (Parser)
 import Text.ParserCombinators.Parsec
-import Text.Parsec.Char (char, digit, string, letter, space)
 import Control.Monad (void)
 
 --------------------------------------------------------------------------------
 -- unique signal patterns | four digit output value
 -- dac abcf ac fdbcga dgcbae gcbfde fgcbd agfed adcgf cdbgfea | cbfa bcafdg cbfa bafcgd
+
+type OutputValue = String
+
+data Entry = Entry {
+  signalPatterns :: [String],
+  outputValues :: [OutputValue]
+} deriving (Show, Eq)
 
 wordParser :: Parser String
 wordParser = many1 letter <?> "word"
@@ -27,17 +32,30 @@ remainingWords =
     Text.Parsec.try (space >> wordsParser)           -- Found comma?  More cells coming
     <|> return []                     -- No comma?  Return [], no more cells
 
-recordParser :: Parser ([String], [String])
+recordParser :: Parser Entry
 recordParser =
   do
     signalPatterns <- wordsParser
     void (string " | " <?> "delimiter")
     outputValues <- wordsParser
     eof
-    return (signalPatterns, outputValues)
+    return $ Entry signalPatterns outputValues
 
-parseLine :: String -> Either ParseError ([String], [String])
+parseLine :: String -> Either ParseError Entry
 parseLine = parse recordParser ""
+
+--------------------------------------------------------------------------------
+
+is1478 :: OutputValue -> Bool
+is1478 s = go $ length s where
+  go 2 = True -- 1
+  go 4 = True -- 4
+  go 3 = True -- 7
+  go 7 = True -- 8
+  go _ = False
+
+countOf1478 :: [Entry] -> Int
+countOf1478 = length . filter is1478 . concatMap outputValues
 
 --------------------------------------------------------------------------------
 
@@ -46,6 +64,8 @@ run = do
   content <- readFile "input/day8.txt"
   let lines' = lines content
   let records = mapM parseLine lines'
-  putStrLn $ "Records: " ++ show records
-
-  return ()
+  case records of
+    Left e -> putStrLn $ "Failed to parse: " ++ show e
+    Right entries ->
+      let count' = countOf1478 entries in
+      putStrLn $ "Count of 1, 4, 7 or 8: " ++ show count'
